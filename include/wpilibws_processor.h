4053b6e4ec5b7c0479c4e2c8b979f13f310d9ab3
Zhiquan Yeo <zyeo8@bloomberg.net>
2023-04-18 09:53:13 -0400

    WIP: WS Message processing and robot refactor

---
+++ include/wpilibws_processor.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <functional>
+#include <ArduinoJson.h>
+
+namespace wpilibws {
+  typedef std::function<void(int channel, double value)> PWMCallback;
+  typedef std::function<void()> DSGenericCallback;
+  typedef std::function<void(bool dsEnabled)> DSEnabledCallback;
+
+  class WPILibWSProcessor {
+    public:
+      WPILibWSProcessor();
+      void processMessage(JsonDocument& jsonMsg);
+
+      void onPWMMessage(const PWMCallback callback);
+      void onDSGenericMessage(const DSGenericCallback callback);
+      void onDSEnabledMessage(const DSEnabledCallback callback);
+
+    private:
+      void handlePWMMessage(JsonDocument& pwmMsg);
+      void handleDSMessage(JsonDocument& dsMsg);
+
+      PWMCallback _pwmCallback;
+      DSGenericCallback _dsGenericCallback;
+      DSEnabledCallback _dsEnabledCallback;
+  };
+}
